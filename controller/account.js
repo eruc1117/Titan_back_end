b0d4df14dcd77dd1a076d4a2af6293bd2ae4f86a
eruc <eruc101010@gmail.com>
2022-12-19 22:59:19 +0800

    feat: add JWT token in response message

--- controller/account.js
+++ controller/account.js
@@ -1,36 +1,47 @@
 const pool = require("../config/mySql/connect")
 const bcrypt = require('bcrypt')
+const jwt = require ("jsonwebtoken")
+const path = require('path')
+const envPath = path.join(__dirname, '../.env')
+require('dotenv').config({ path: envPath })
 
 const accountController = {
     login: async (req, res) => {
         try {
         const promisePool = pool.promise()
         const [account, password] = [req.body.account, req.body.password]
-        const sqlCmd = `SELECT password FROM user WHERE account = '${account}'`
-        const preSqlResult = await promisePool.query(sqlCmd)
-        const dbPassword = preSqlResult[0][0]["password"]
+        const sqlCmd = `SELECT id, password FROM user WHERE account = '${account}'`
+        const preSqlResult = (await promisePool.query(sqlCmd))[0]
+        if (0 === preSqlResult.length) {
+            throw new Error ("無此帳號！")
+        }
+        const dbPassword = preSqlResult[0]["password"]
         const chkResult = await bcrypt.compare(password, dbPassword)
         let resMsg = {
             "status": chkResult ? 200 : 400,
             message: {}
         }
         if (chkResult) {
+
+            const token = jwt.sign({id: preSqlResult[0]["id"]}, process.env.secret, { expiresIn: '1d' })
+            
             resMsg.message = {
                 "login": "true",
                 "workState": "start",
+                "token": token,
                 "admin": false
             }
         } else {
             resMsg.message = {
                 "login": "false",
-                "workState": "start",
+                "workState": "logout",
                 "admin": false
             }
         }
-        resMsg = JSON.stringify(resMsg)
-        res.json(resMsg)
+        res.status(resMsg.status).json(resMsg)
         } catch (err) {
             console.log(err)
+            res.status(400).json({err: "err"})
         }
     }
 }
