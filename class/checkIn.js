266b7a980602ac6f6461093e7319c614fd0a739b
eruc <eruc101010@gmail.com>
2022-12-20 23:52:00 +0800

    feat: add function to get department's location

--- class/checkIn.js
+++ class/checkIn.js
@@ -1,14 +1,25 @@
+const pool = require("../config/mySql/connect")
+
 class CheckIn {
     constructor (depId) {
-        this.depLocation = (this.getInfo(depId))["location"]
+        this.id = depId
     }
 
-    async getInfo (depId) {
-        // let sql = ""
-        // let result = sql
-        // return {
-        //     location: [123, 1233]
-        // }
+    async getLocationInfo () {
+        try {
+            const promisePool = pool.promise()
+            let sqlCmd = `SELECT latitude, longitude FROM Titan.location `
+            sqlCmd += `WHERE depId = ${this.id}`
+            const result = await promisePool.query(sqlCmd)
+            if (0 === (result[0]).length) {
+                throw new Error ("Department Id 錯誤!")
+            }
+            return {
+                location:result[0][0]
+            }
+    } catch (err) {
+        console.log(err)
+    }
     }
 
     async gpsPunch (userId, time, location) {
