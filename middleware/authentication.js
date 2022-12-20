e4e6b1834bcee076c9107c426035591d80d129d1
eruc <eruc101010@gmail.com>
2022-12-20 21:33:42 +0800

    feat: add Authentication middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const path = require('path')
+const envPath = path.join(__dirname, '../.env')
+require('dotenv').config({ path: envPath })
+
+const authentication = async (req, res, next) => {
+    try {
+        const authToken = (req.headers['authorization']).replace("Bearer ", "")
+        const decoded = await jwt.verify(authToken, process.env.secret)
+        req.middlewarePassData = {
+            userId : decoded.id
+        }
+        next()
+    } catch (err) {
+        res.send("Token is wrong !!")
+    }
+}
+
+module.exports = authentication
--- routes/module/account.js
+++ routes/module/account.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const accountController = require("../../controller/account")
+const authentication = require("../../middleware/authentication")
 
 router.post("/login", accountController.login)
+router.use(authentication)
+
 
 module.exports = router
