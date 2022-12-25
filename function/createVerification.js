7c8ab9b0456865bf1b857bbf0b6a7bc8268aef98
eruc <eruc101010@gmail.com>
2022-12-25 14:58:21 +0800

    refactor: Change the method of obtaining the verification code Id

--- function/createVerification.js
+++ function/createVerification.js
@@ -4,13 +4,9 @@ const createVerification = async () => {
   try {
     const randomNum = Math.floor(Math.random() * 999);
     const promisePool = pool.promise();
-    let insertSql = `
-      INSERT INTO verification (verification) VALUES (${randomNum});`;
-    //LAST_INSERT_ID()
-    let sqlCmd = `SELECT id FROM verification WHERE verification = ${randomNum}`;
-    await promisePool.query(insertSql);
-    const result = await promisePool.query(sqlCmd);
-    return [result[0][0]["id"], randomNum];
+    let insertSql = `INSERT INTO verification (verification) VALUES (${randomNum});`;
+    const result = (await promisePool.query(insertSql))[0];
+    return [result.insertId, randomNum];
   } catch (err) {
     console.log(err);
   }
