23205a5952f99973a3e1669ee166c3d6b09af1b8
eruc <eruc101010@gmail.com>
2023-01-03 20:41:44 +0800

    feat: edit Heroku PORT

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const path = require('path')
-const envPath = path.join(__dirname, './.env')
-require('dotenv').config({ path: envPath })
-const cors = require("cors")
-const express = require("express")
-const app = express()
-const port = process.env.expressPort
-const routes = require("./routes/index")
+const path = require("path");
+const envPath = path.join(__dirname, "./.env");
+require("dotenv").config({ path: envPath });
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+const routes = require("./routes/index");
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Web Service listening on port ${port}`)
-})
+  console.log(`Web Service listening on port ${port}`);
+});
