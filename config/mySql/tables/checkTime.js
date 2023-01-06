6f64a4d502347b7829b838bbae65395fa5f20bf4
eruc <eruc101010@gmail.com>
2023-01-06 22:10:23 +0800

    feat: change checkIn table

--- config/mySql/tables/checkTime.js
+++ config/mySql/tables/checkTime.js
@@ -1,21 +1,21 @@
-const pool = require("../connect")
+const pool = require("../connect");
 
-initCheckTime()
+initCheckTime();
 
-async function initCheckTime () {
-    try {
-        const promisePool = pool.promise()
-        let tableSql ="CREATE TABLE IF NOT EXISTS checkTime ("
-        tableSql += "id int NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-        tableSql += "userId int NOT NULL,"
-        tableSql += "start DATETIME,"
-        tableSql += "end DATETIME,"
-        tableSql += "FOREIGN KEY (userId)"
-        tableSql += "REFERENCES user(id))"
-        const [rows,fields] = await promisePool.query(tableSql)
-    } catch (err) {
-        console.log(err)
-    } finally {
-        pool.end()
-    }
+async function initCheckTime() {
+  try {
+    const promisePool = pool.promise();
+    let tableSql = "CREATE TABLE IF NOT EXISTS checkTime (";
+    tableSql += "id int NOT NULL AUTO_INCREMENT PRIMARY KEY,";
+    tableSql += "userId int NOT NULL,";
+    tableSql += "start INT,";
+    tableSql += "end INT,";
+    tableSql += "FOREIGN KEY (userId)";
+    tableSql += "REFERENCES user(id))";
+    const [rows, fields] = await promisePool.query(tableSql);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    pool.end();
+  }
 }
