50f0d37a12bfd1b42fc14d5b6fdb60ce2dd882cb
eruc <eruc101010@gmail.com>
2023-01-09 22:10:05 +0800

    feat: update mysql procedure

--- sqlFile/checkTime.sql
+++ sqlFile/checkTime.sql
@@ -2,17 +2,17 @@ DELIMITER //
 CREATE PROCEDURE insertCheckTime(IN id INT, IN startTime datetime, IN endTime datetime)
 BEGIN
   DECLARE idExist INT DEFAULT 0;
-  SET idExist = (SELECT COUNT(id) FROM Titan.checkTime WHERE userId = id AND start > startTime AND start < endTime);
+  SET idExist = (SELECT COUNT(id) FROM checkTime WHERE userId = id AND start > UNIX_TIMESTAMP(startTime) AND start < UNIX_TIMESTAMP(endTime));
   IF (idExist > 0)
   THEN
   BEGIN 
-	UPDATE Titan.checkTime SET end = NOW() WHERE userId = id AND start > startTime AND start < endTime;
+	UPDATE checkTime SET end = UNIX_TIMESTAMP(NOW()) WHERE userId = id AND start > UNIX_TIMESTAMP(startTime) AND start < UNIX_TIMESTAMP(endTime);
   END;
   ELSE
   BEGIN
-	INSERT INTO Titan.checkTime (userId, start) VALUES (id, NOW());
+	INSERT INTO checkTime (userId, start) VALUES (id, UNIX_TIMESTAMP(NOW()));
   END;
   END IF;
-  SELECT * FROM Titan.checkTime WHERE userId = id AND start > startTime AND start < endTime;
+  SELECT userId, CONVERT_TZ(FROM_UNIXTIME(start), 'SYSTEM', '+8:00') AS start, CONVERT_TZ(FROM_UNIXTIME(end), 'SYSTEM', '+8:00') AS end FROM checkTime WHERE userId = id AND start > UNIX_TIMESTAMP(startTime) AND start < UNIX_TIMESTAMP(endTime);
 END//
 DELIMITER ;
